b46d734637605cec58aeb8becd75e972816a37ca
中村祐馬 <yuma.nakamura.0309@gmail.com>
2023-02-22 19:25:19 +0900

    アプリ全体の設定を修正

--- CorporateSite_Internal_LinkTree/settings.py
+++ CorporateSite_Internal_LinkTree/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-66$-=v2#921yl4g3he_^x#+wa3l+)v7d@@5u_ppa(p*i+$xp&+'
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -78,8 +81,8 @@ WSGI_APPLICATION = 'CorporateSite_Internal_LinkTree.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -126,7 +129,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = (
-
     os.path.join(BASE_DIR, 'static'),
-
 )
